84a0d3c98b2685eca9ea1c604b49a48b8deea270
Itzmecp <121564783+Cpmodzyt@users.noreply.github.com>
2024-11-30 12:19:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,19 +25,19 @@ load_dotenv()
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
-        self.API_HASH: str = os.environ.get("API_HASH", None)
-        self.SESSION: str = os.environ.get("SESSION", None)
-        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", None)
+        self.API_ID: str = os.environ.get("API_ID", "28889770")
+        self.API_HASH: str = os.environ.get("API_HASH", "0ecaead60f942ee1c8415bcb336e7329")
+        self.SESSION: str = os.environ.get("SESSION", "BQG40qoAaBiB8o2t-0tA2wtXT2vxhvIwKv3yxWC_4sdS06npatNzW8fNwtwL7cQhh_8WZkPvuPiMywvIlmd_YF4IrtLpfomgAO5dvsQa2jgAehct65FgmZ-fcHwDpXtYD-kSWDlgDwwgHMGILza7v0BSavDMQeSa_oCPKqSqgDpUjwFtAxvKgInXA34kbf-eXwKiiPZMiaNKEVQ0Q37tOmuyDK0rBs-vrSxJMAaiDtbmhfciXdCRIJggSAJ5mKSzlsdXX_GS7hm3SLrt-5FMgOKUrvg4_S11Iy-SDjIRcexjbp8lrQKVRuTva4qhKy-OG4I3SaD3RuqBoYJiX1gYaUGYPsNfdwAAAAHBc6w6AA")
+        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "7701703310:AAGXi1-8_co_aI3yYljoGX_MZBRboQqtfFE")
         self.SUDOERS: list = [
-            int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
+            int(id) for id in os.environ.get("SUDOERS", "7717701360").split() if id.isnumeric()
         ]
         if not self.SESSION or not self.API_ID or not self.API_HASH:
             print("ERROR: SESSION, API_ID and API_HASH is required!")
             quit(0)
         self.SPOTIFY: bool = False
         self.QUALITY: str = os.environ.get("QUALITY", "high").lower()
-        self.PREFIXES: list = os.environ.get("PREFIX", "!").split()
+        self.PREFIXES: list = os.environ.get("PREFIX", "/").split()
         self.LANGUAGE: str = os.environ.get("LANGUAGE", "en").lower()
         self.STREAM_MODE: str = (
             "audio"
